7ebc52e6a02861f8a7901511ad6af214f9527f30
MehmetTurkerofRivia <mertmemo69@gmail.com>
2023-09-13 20:32:15 +0300

    Main menu ve settings tuşları eklendi vs.

---
+++ rog-like-game/Assets/Scripts/LoadScreen.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+
+public class LoadScreen : MonoBehaviour
+{
+    public Slider progressBar;
+    public void Load(int start)
+    {
+        StartCoroutine(startLoading(start));
+    }
+
+    IEnumerator startLoading(int start)
+    {
+        yield return new WaitForSeconds(3);
+        AsyncOperation async = SceneManager.LoadSceneAsync(start);
+        while (!async.isDone)
+        {
+            progressBar.value = async.progress;
+            yield return null;
+        }
+
+       
+    }
+}
